f5bc69dad243632985b373adc39a55fd4351c15b
Johnvent <johnvent.otech@gmail.com>
2023-10-04 14:55:32 -0700

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc ; i++)
